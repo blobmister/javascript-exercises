dc871a17ef2f1124efa768a9bf7c51ede339f12a
Krish Gupta <179193922+blobmister@users.noreply.github.com>
2024-12-02 20:43:53 +1100

    Completed reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,25 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let strAr = new Array();
+
+    // Generating array of characters
+    for (let i = 0; i < str.length; i++) {
+        strAr.push(String(str).charAt(i));
+    }
+
+    let reverseAr = new Array();
+
+    // Reversing the array
+    for (let i = 0; i < str.length; i++) {
+        reverseAr[i] = strAr[strAr.length - (i + 1)];
+    }
+
+    //Putting array back to string
+    let outStr = "";
+    for (let i = 0; i < reverseAr.length; i++) {
+        outStr += reverseAr[i];
+    }
+
+    return outStr;
 
 };
 
