7125075cfdf34a213586e9ce18213155e7cd09e5
Krish Gupta <179193922+blobmister@users.noreply.github.com>
2024-12-02 20:25:53 +1100

    completed repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let outstr = "";
+    if (num < 0) {
+        return "ERROR";
+    }
 
+    for (let i = 0; i < num; i++) {
+        outstr += str;
+    }
+
+    return outstr;
 };
 
 // Do not edit below this line
