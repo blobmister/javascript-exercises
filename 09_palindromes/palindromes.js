16ba9b7084d5a410bb04c01a2857f9bbf105bab7
Krish Gupta <179193922+blobmister@users.noreply.github.com>
2024-12-03 14:43:54 +1100

    Completed palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let punctuation = /[\.,?!]/g;
+    word = word.replace(punctuation, "").replace(/ /g, "").toLowerCase();
 
+    for (let i = 0; i < word.length; i++) {
+        if(!(word.charAt(i) == word.charAt(word.length - 1 - i))) {
+            return false;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
