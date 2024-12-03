b3d5385e41aab973252af40cf40615d6e8093a5b
Krish Gupta <179193922+blobmister@users.noreply.github.com>
2024-12-03 14:23:11 +1100

    Completed leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // Year must be an integer
+    if(!Number.isInteger(year)) {
+        return "ERROR";
 
+    }
+
+    if (year % 4 == 0) {
+        if (!(year % 100 == 0) || (year % 400 == 0)) {
+            return true;
+        }
+    }
+
+    return false;
 };
 
 // Do not edit below this line
