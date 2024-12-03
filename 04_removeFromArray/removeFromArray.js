f0596e4fa002d600f32beb6cfb1b8d369d9e36ef
Krish Gupta <179193922+blobmister@users.noreply.github.com>
2024-12-03 14:04:56 +1100

    Completed removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(ar) {
+    outAr = new Array();
+
+    for (let i = 0; i < ar.length; i++) {
+        let isntDupe = true;
+        for (let j = 1; j < arguments.length; j++) {
+            if (ar[i] === arguments[j]) {
+                isntDupe = false;
+                break;
+            }
+        }
+
+        if (isntDupe) {
+            outAr.push(ar[i]);
+        }
+    }
+
+    return outAr;
 };
 
 // Do not edit below this line
