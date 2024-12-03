78284d847861ed3adf5d8ad86c4a56e15bc04fea
Krish Gupta <179193922+blobmister@users.noreply.github.com>
2024-12-03 14:20:04 +1100

    Completed sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
 
+    // Check if input was valid
+    if (!(Number.isInteger(a) && Number.isInteger(b))) {
+        return "ERROR";
+    }
+
+    if(a < 0 || b < 0) {
+        return "ERROR";
+    }
+
+    // Swap variables if larger number is first
+    if (b < a) {
+        let c = a;
+        a = b;
+        b = c;
+    }
+
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
