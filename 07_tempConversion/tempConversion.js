e6df397ab858495a92e8350cc20daacd33a35cd7
Krish Gupta <179193922+blobmister@users.noreply.github.com>
2024-12-03 14:28:16 +1100

    Completed tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  let cel = (far - 32) * 5/9;
+
+  cel = Math.round(cel * 10) / 10;
+
+  return cel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let far = (cel * 9/5) + 32;
+
+  far = Math.round(far * 10)/10;
+
+  return far;
 };
 
 // Do not edit below this line
