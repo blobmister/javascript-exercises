5418b8fef7bb34f731b481eae615647a251c2fa4
Krish Gupta <179193922+blobmister@users.noreply.github.com>
2024-12-03 14:34:22 +1100

    Completed Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  let sum = 0;
+	for(let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let product = 1;
+	for(let i = 0; i < nums.length; i++) {
+    product = product * nums[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, x) {
+	let result = 1;
+
+  for (let i = 0; i < x; i++) {
+    result = result * a;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let result = 1;
+  
+  for (let i = 1; i <= a; i++ ) {
+    result = result * i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
