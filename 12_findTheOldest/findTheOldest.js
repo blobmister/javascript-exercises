02af2f7055ef1588bb40d67577c522607940ec22
Krish Gupta <179193922+blobmister@users.noreply.github.com>
2024-12-03 15:09:07 +1100

    Completed findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestPerson = people[0];
 
+    function age(person) {
+        if(person.yearOfDeath != null) {
+            return person.yearOfDeath - person.yearOfBirth
+        } else {
+            return 2024 - person.yearOfBirth;
+        }
+    }
+
+    for (let i = 1; i < people.length; i++) {
+        if(age(oldestPerson) < age(people[i])) {
+            oldestPerson = people[i];
+        }
+    }
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
